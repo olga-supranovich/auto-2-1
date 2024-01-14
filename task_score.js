fe52faeb386aaf8861e06ef6f1501454c6240eb3
olga-supranovich <149422327+olga-supranovich@users.noreply.github.com>
2024-01-14 19:41:32 +0100

    Created task_score.js for task 2.1.3

---
+++ task_score.js
@@ -0,0 +1,12 @@
+let scoreModule0 = 100; //баллы модуля Введение
+let scoreModule1 = 200; //баллы модуля git
+let scoreModule2 = 300; //баллы модуля js
+
+let averageScore = (scoreModule0 + scoreModule1 + scoreModule2) / 3;
+
+let studentName = "Olga";
+let studentSurname = "Supranovich";
+
+console.log(
+  `Средний балл по каждому модулю для студента ${studentName} ${studentSurname}: ${averageScore}`
+);
