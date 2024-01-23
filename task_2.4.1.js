3e1ecf1d5891636ca31ccb69138f8dcb986d017d
olga-supranovich <149422327+olga-supranovich@users.noreply.github.com>
2024-01-23 18:33:25 +0100

    Created task_2.4.1.js for task 2.4.1

---
+++ task_2.4.1.js
@@ -0,0 +1,48 @@
+let expencesExamples = [
+  {
+    yearlyExpences: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpences: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpences: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+
+function calculateSum(arr) {
+  let sum = 0;
+  arr.forEach((value) => {
+    if (value > 1000) {
+      sum += value;
+    }
+  });
+  return sum;
+}
+
+let expectedResult = [40590, 148200, 25709];
+
+function testCalculateSum() {
+  expencesExamples.forEach((value, index) => {
+    let calculatedResult = calculateSum(value.yearlyExpences);
+    if (calculatedResult === expectedResult[index]) {
+      console.log(`Test ${index + 1}: Calculated sum is correct`);
+    } else {
+      console.log(
+        `${
+          index + 1
+        } Calculated sum: ${calculatedResult} is not equal to expected: ${
+          expectedResult[index]
+        }`
+      );
+    }
+  });
+}
+
+testCalculateSum();
